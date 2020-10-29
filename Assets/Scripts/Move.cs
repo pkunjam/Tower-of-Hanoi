02eaa0bc133394447e3465f7e71b2250ce632e27
pkunjam <punit17100@iiitnr.edu.in>
2020-10-30 02:06:40 +0530

    working on move script

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    GameObject selectedDisk = null;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit raycastHit;
+            Ray rayCast = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(rayCast, out raycastHit) && raycastHit.collider.CompareTag("Disk"))
+            {
+                selectedDisk = raycastHit.collider.gameObject;
+            }
+        }
+        
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,23 +5,25 @@ using System.Collections;
     public class PlayerMovement : MonoBehaviour
     {
 
-    public CharacterController controller;
+        public CharacterController controller;
+        public float speed;
+        Vector2 rotation = Vector2.zero;
 
-    public float speed;
-    Vector2 rotation = Vector2.zero;
+        void Update()
+        {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
 
-    void Update()
-    {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+            Vector3 move = transform.right * x + transform.forward * z;
 
-        Vector3 move = transform.right * x + transform.forward * z;
+            controller.Move(move * speed * Time.deltaTime);
 
-        controller.Move(move * speed * Time.deltaTime);
+            if (Input.GetMouseButton(1))
+            {
+                rotation.y += Input.GetAxis("Mouse X");
+                Camera.main.transform.localRotation = Quaternion.Euler(rotation.x * speed, rotation.y * speed, 0);
+            }
 
-        rotation.y += Input.GetAxis("Mouse X");
-        Camera.main.transform.localRotation = Quaternion.Euler(rotation.x * speed, rotation.y * speed, 0);
-
-    }
+        }
 
 }
