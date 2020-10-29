f841d4394babc5231e061b576d176a60d5ebd717
pkunjam <punit17100@iiitnr.edu.in>
2020-10-29 13:01:04 +0530

    Environmental and Player movement setup

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+    public class PlayerMovement : MonoBehaviour
+    {
+
+    public CharacterController controller;
+
+    public float speed;
+    Vector2 rotation = Vector2.zero;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+        Camera.main.transform.localRotation = Quaternion.Euler(rotation.x * speed, rotation.y * speed, 0);
+
+    }
+
+}
